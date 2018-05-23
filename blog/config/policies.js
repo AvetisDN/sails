40e7535d58605f5e9b66672a4926cef40e95dbf3
AvetisDN <avetisdn@gmail.com>
2018-05-23 15:47:22 +0300

    1

--- blog/config/policies.js
+++ blog/config/policies.js
@@ -10,13 +10,32 @@
 
 module.exports.policies = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Default policy for all controllers and actions, unless overridden.       *
-  * (`true` allows public access)                                            *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+     *                                                                          *
+     * Default policy for all controllers and actions, unless overridden.       *
+     * (`true` allows public access)                                            *
+     *                                                                          *
+     ***************************************************************************/
+    '*': true,
 
-  // '*': true,
+    AdminController: {
+        '*': 'admin'
+    },
+
+    UserController: {
+        create: 'admin'
+    },
+
+    PostController: {
+        // То что могут видеть все
+        index: true,
+        page: true,
+        watch: true,
+
+        // То что может только админ
+        create: 'admin',
+        update: 'admin',
+        delete: 'admin',
+    }
 
 };
